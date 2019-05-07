2143d5266c5d32b36363aec68b15051c16f15816
dmckinley3 <dmckinley3@studentmail.nait.ca>
2019-05-06 18:47:14 -0600

    create my first class

--- OOPsSolution/OOPsReview/Program.cs
+++ OOPsSolution/OOPsReview/Program.cs
@@ -10,9 +10,40 @@ namespace OOPsReview
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            //Console is a reference to a system static class
+            //static classes are used by typing the class name
+            //static classes do not store variable data
+            //static classes are used by everyone
 
+            //methods of a static class will be placed after
+            //the object dot operator
+
+            Console.WriteLine("Hello World");
+
+            //create an instance of a non-static class
+            //If you create a declareative using JUST the 
+            //      class name then this is only a pointer
+            //      that can hold an instance of the class
+            //      specified
+            //current value of aVariable is null
+
+            myData aVariable = null;
+            //if you use the new command with the class 
+            //      the new command will physically create
+            //      an instance of the class
+            myData anInstance = new myData();
+
+            if(aVariable ==  null)
+            {
+                Console.WriteLine("aVariable has nothing in it");
+            }
+
+            if(anInstance ==  null)
+            {
+                Console.WriteLine("anInstance has nothing in it");
+            }
             Console.ReadLine();
-        }
-    }
-}
+        }//eom
+    }//eoc
+
+}//eon
